7b43f4bd17cbd68945ee0605af3d5c2a92510f9e
sevdaerol <sevda.erol93@gmail.com>
2021-08-20 22:52:34 +0200

    cart

---
+++ frontend/cart.js
@@ -0,0 +1,8 @@
+let cart = document.querySelector(".cart_filled_details");
+let copyLocalStorage = JSON.parse(localStorage.getItem("articles")); //copier le local storage
+
+main(); //function call
+
+function main() {
+
+} 
